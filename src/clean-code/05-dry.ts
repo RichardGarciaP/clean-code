6e1cba1ae7a1f8273a2d1f01f7b4b160a2e21606
FreddyBenitez <68798656+FreddyBenitez@users.noreply.github.com>
2024-03-17 23:23:34 -0500

    principio DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,16 +7,37 @@ class Product {
     public size: Size = ""
   ) {}
 
+  isProductReady(): boolean {
+    for (const key in this) {
+      switch (typeof this[key]) {
+        case "string":
+          if ((<string>this[key]).length <= 0)
+            throw new Error(`${key} is empty`);
+          break;
+
+        case "number":
+          if (<number>this[key] <= 0) throw new Error(`${key} is zero`);
+          break;
+
+        default:
+          throw new Error(`${typeof this[key]} is not valid`);
+      }
+    }
+    return true;
+  }
+
   toString() {
-    if (this.name.length <= 0) throw Error("name is empty");
-    if (this.price <= 0) throw Error("price is zero");
-    if (this.size.length <= 0) throw Error("size is empty");
+    // if (this.name.length <= 0) throw Error("name is empty");
+    // if (this.price <= 0) throw Error("price is zero");
+    // if (this.size.length <= 0) throw Error("size is empty");
+
+    if (!this.isProductReady) return;
 
     return `${this.name} (${this.price}), ${this.size}`;
   }
 }
 
 (() => {
-  const bluePants = new Product("Blue Large Pants");
+  const bluePants = new Product("Blue Large Pants", 10, "S");
   console.log(bluePants.toString());
 })();
